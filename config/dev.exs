9768b77a0657edb95f50b70dba155daaf690e6d7
Yusef Mosiah Nathanson <yusef@mosiah.org>
2023-03-15 17:38:37 -0400

    project setup

--- config/dev.exs
+++ config/dev.exs
@@ -19,14 +19,15 @@ config :blog, Blog.Repo,
 config :blog, BlogWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {127, 0, 0, 1}, port: 4040],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
   secret_key_base: "X1aMmiW7ZzO3NPmNnwu8x7n8oMfjd8s/w+oOG/7v1+M7eV1DOFA0+6spCWhYyBO0",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
