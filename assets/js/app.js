1e764a8ee8fd4d4fe473ea4838e67c18dbc8a650
Yusef Mosiah Nathanson <yusef@mosiah.org>
2023-03-19 14:44:10 -0400

    :content textarea grows with content length

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,6 +20,10 @@
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
+
+// Custom JS
+import "./textarea_autoresize";
+
 // Establish Phoenix Socket and LiveView configuration.
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
@@ -41,4 +45,3 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
-
--- lib/blog_web/views/post_view.ex
+++ lib/blog_web/views/post_view.ex
@@ -1,3 +1,16 @@
 defmodule BlogWeb.PostView do
   use BlogWeb, :view
+
+  def render_code_blocks(content) do
+    content
+    |> String.split("```")
+    |> Enum.map(fn segment, index ->
+      if Integer.is_even(index) do
+        "<pre class=\"code-frame\">#{Phoenix.HTML.raw(segment)}</pre>"
+      else
+        segment
+      end
+    end)
+    |> Enum.join("")
+  end
 end
