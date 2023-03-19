5e78117dac43f4b523b986df58124895a077c63e
Yusef Mosiah Nathanson <yusef@mosiah.org>
2023-03-18 23:37:59 -0400

    1st draft of the ui done with help from gpt-4

--- lib/blog_web/controllers/page_controller.ex
+++ lib/blog_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule BlogWeb.PageController do
   use BlogWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    redirect(conn, to: Routes.post_path(conn, :index))
   end
 end
