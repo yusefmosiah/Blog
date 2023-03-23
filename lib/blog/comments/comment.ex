485cb63915fd41aacadef8a2e79a752c1d638851
Steve Freeman <steve@indieit.com>
2023-03-23 13:27:35 -0500

    making comments work

--- lib/blog/comments/comment.ex
+++ lib/blog/comments/comment.ex
@@ -12,7 +12,7 @@ defmodule Blog.Comments.Comment do
   @doc false
   def changeset(comment, attrs) do
     comment
-    |> cast(attrs, [:content])
+    |> cast(attrs, [:content, :post_id])
     |> validate_required([:content])
   end
 end
--- lib/blog_web/controllers/comment_controller.ex
+++ lib/blog_web/controllers/comment_controller.ex
@@ -15,14 +15,16 @@ defmodule BlogWeb.CommentController do
   end
 
   def create(conn, %{"comment" => comment_params}) do
+    IO.inspect(comment_params, label: "comment params")
     case Comments.create_comment(comment_params) do
       {:ok, comment} ->
         conn
         |> put_flash(:info, "Comment created successfully.")
-        |> redirect(to: Routes.comment_path(conn, :show, comment))
+        |> redirect(to: Routes.post_path(conn, :show, comment_params["post_id"]))
 
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
+        #redirect(conn, to: Routes.post_path(conn, :show, comment_params["post_id"]))
     end
   end
 
