f3214f118fdee9e102d32aeb501161151dcf1418
Yusef Mosiah Nathanson <yusef@mosiah.org>
2023-03-24 15:14:45 -0400

    41 tests pass, all green

--- test/blog_web/controllers/comment_controller_test.exs
+++ test/blog_web/controllers/comment_controller_test.exs
@@ -4,10 +4,6 @@ defmodule BlogWeb.CommentControllerTest do
   import Blog.CommentsFixtures
   import Blog.PostsFixtures
 
-  @create_attrs %{content: "some content"}
-  @update_attrs %{content: "some updated content"}
-  @invalid_attrs %{content: nil}
-
   describe "index" do
     test "lists all comments", %{conn: conn} do
       conn = get(conn, Routes.comment_path(conn, :index))
@@ -15,46 +11,46 @@ defmodule BlogWeb.CommentControllerTest do
     end
   end
 
-  describe "new comment" do
-    test "renders form", %{conn: conn} do
-      conn = get(conn, Routes.comment_path(conn, :new))
-      assert html_response(conn, 200) =~ "New Comment"
-    end
-  end
-
   describe "create comment" do
     test "create comment for associated post", %{conn: conn} do
       post = post_fixture()
-      comment = comment_fixture(post_id: post.id) |> Blog.Repo.preload(:post)
-      IO.inspect(comment, label: "XXXXXXXcomment")
 
-      conn = post(conn, Routes.comment_path(conn, :create), comment: %{comment: comment})
+      conn =
+        post(conn, Routes.comment_path(conn, :create, post.id),
+          comment: %{post_id: post.id, content: "some content"}
+        )
+
       assert %{id: id} = redirected_params(conn)
-      assert redirected_to(conn) == Routes.comment_path(conn, :show, id)
+      assert redirected_to(conn) == Routes.post_path(conn, :show, id)
 
-      conn = get(conn, Routes.comment_path(conn, :show, id))
-      assert html_response(conn, 200) =~ "Show Comment"
+      conn = get(conn, Routes.post_path(conn, :show, id))
+      assert html_response(conn, 200) =~ "Comment created successfully"
     end
 
     test "redirects to show when data is valid", %{conn: conn} do
       post = post_fixture()
-      comment = comment_fixture(post_id: post.id)
 
-      conn = post(conn, Routes.comment_path(conn, :create), comment: %{comment: comment})
+      conn =
+        post(conn, Routes.comment_path(conn, :create, post.id),
+          comment: %{post_id: post.id, content: "some content"}
+        )
 
       assert %{id: id} = redirected_params(conn)
-      assert redirected_to(conn) == Routes.comment_path(conn, :show, id)
+      assert redirected_to(conn) == Routes.post_path(conn, :show, id)
 
-      conn = get(conn, Routes.comment_path(conn, :show, id))
-      assert html_response(conn, 200) =~ "Show Comment"
+      conn = get(conn, Routes.post_path(conn, :show, id))
+      assert html_response(conn, 200) =~ "Comment created successfully"
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
       post = post_fixture()
-      {:error, comment} = comment_fixture(%{post_id: post.id, content: nil})
 
-      conn = post(conn, Routes.comment_path(conn, :create), comment: %{comment: comment})
-      assert html_response(conn, 200) =~ "New Comment"
+      conn =
+        post(conn, Routes.comment_path(conn, :create, post.id),
+          comment: %{post_id: post.id, content: nil}
+        )
+
+      assert html_response(conn, 200) =~ "some title"
     end
   end
 
@@ -71,9 +67,6 @@ defmodule BlogWeb.CommentControllerTest do
     setup [:create_comment]
 
     test "redirects when data is valid", %{conn: conn, comment: comment} do
-      post = post_fixture()
-      comment = comment_fixture(post_id: post.id)
-
       conn = put(conn, Routes.comment_path(conn, :update, comment), comment: %{comment: comment})
       assert redirected_to(conn) == Routes.comment_path(conn, :show, comment)
 
@@ -82,7 +75,7 @@ defmodule BlogWeb.CommentControllerTest do
     end
 
     test "renders errors when data is invalid", %{conn: conn, comment: comment} do
-      conn = put(conn, Routes.comment_path(conn, :update, comment), comment: @invalid_attrs)
+      conn = put(conn, Routes.comment_path(conn, :update, comment), comment: %{content: nil})
       assert html_response(conn, 200) =~ "Edit Comment"
     end
   end
@@ -100,7 +93,7 @@ defmodule BlogWeb.CommentControllerTest do
     end
   end
 
-  def create_comment(_) do
+  defp create_comment(_) do
     post = post_fixture()
     comment = comment_fixture(post_id: post.id)
     %{comment: comment}
