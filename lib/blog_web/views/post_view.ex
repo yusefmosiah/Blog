d235fb8f63777c90efd0df8e4665e5e95dd8a5b7
Yusef Mosiah Nathanson <yusef@mosiah.org>
2023-03-31 10:54:38 -0400

    minor tweaks

--- lib/blog_web/views/post_view.ex
+++ lib/blog_web/views/post_view.ex
@@ -4,4 +4,13 @@ defmodule BlogWeb.PostView do
   def tag_options do
     Blog.Tags.list_tags() |> Enum.map(fn tag -> {tag.name, tag.id} end)
   end
+
+  def post_content_preview(post) do
+    content = post.content
+    |> String.graphemes()
+    |> Enum.take(200)
+    |> Enum.join()
+
+    content <> "..."
+  end
 end
